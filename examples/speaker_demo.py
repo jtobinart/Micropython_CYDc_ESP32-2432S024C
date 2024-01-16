d6d256db31b7d694ff37261ff34fc331f04c122b
JT <68863931+jtobinart@users.noreply.github.com>
2024-01-15 22:38:23 -0500

    Add files via upload

---
+++ examples/speaker_demo.py
@@ -0,0 +1,33 @@
+# CYDc Library
+# Tags: Micropython Cheap Yellow Device DIYmall ESP32-2432S028R
+# Last Updated: Dec. 2, 2023
+# Author(s): James Tobin
+# License: MIT
+# https://github.com/jtobinart/MicroPython_CYD_ESP32-2432S028R
+
+import time
+from cydc import CYD
+
+cyd = CYD()
+
+cyd.display.fill_rectangle(0, 0, cyd.display.width-1, cyd.display.height-1, cyd.BLUE)
+
+
+duration = 500    # How long to play each note. (in milliseconds)
+pause = 2.0       # How long to pause inbetween each tone. (in seconds)
+
+# Play tone 1
+print("Playing Tone 1")
+cyd.display.draw_text8x8(cyd.display.width // 2 - 56, cyd.display.height // 2 - 4, "Playing Tone 1", cyd.WHITE, background=cyd.BLUE)
+
+cyd.play_tone(220, duration)  # A4 Tone
+time.sleep(pause)
+
+# Play tone 2
+print("Playing Tone 2")
+cyd.display.draw_text8x8(cyd.display.width // 2 - 56, cyd.display.height // 2 - 4, "Playing Tone 2", cyd.WHITE, background=cyd.BLUE)
+
+cyd.play_tone(440, duration)  # C5 Tone
+time.sleep(pause)
+        
+cyd.shutdown()
